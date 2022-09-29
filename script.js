d0f5463eb5cf0f4f6aa89c6d7da615c461301d92
Mirziyod-saidov <saidov.mirziyod@mail.ru>
2022-09-29 10:55:53 +0500

    lesson-3

---
+++ script.js
@@ -0,0 +1,114 @@
+"use strict"
+
+
+// 1-ex
+// let dayNumber = +prompt('enter digit ')
+
+// switch (dayNumber) {
+//    case 1:
+//       console.log('Dushanba');
+//       break;
+//    case 2:
+//       console.log('Seshanba');
+//       break;
+//    case 3:
+//       console.log('Chorshanba');
+//       break;
+//    case 4:
+//       console.log('Payshanba');
+//       break;
+//    case 5:
+//       console.log('Juma');
+//       break;
+//    case 6:
+//       console.log('Shanba');
+//       break;
+//    case 7:
+//       console.log('Yakshanba');
+//       break;
+//       default : console.log('BUNDAY KUN YO\'Q');
+// }
+
+// 2-ex
+// let markValue = +prompt('enter digit')
+
+// switch (markValue) {
+//    case 1:
+//       console.log('yomon');
+//       break;
+//    case 2:
+//       console.log('qoniqarsiz');
+//       break;
+//    case 3:
+//       console.log('qoniqarli');
+//       break;
+//    case 4:
+//       console.log('yaxshi');
+//       break;
+//    case 5:
+//       console.log('a\'lo');
+//       break;
+//       default : console.log('BUNDAY BAHO YO\'Q');
+// }
+
+
+// 3-ex
+// let monthNumber = +prompt('enter digit')
+
+// switch (monthNumber) {
+//    case 1: case 2: case 12:
+//       console.log('QISH');
+//       break;
+//    case 3: case 4: case 5:
+//       console.log('BAHOR');
+//       break;
+//    case 6: case 7: case 8:
+//       console.log('YOZ');
+//       break;
+//    case 9: case 10: case 11:
+//       console.log('KUZ');
+//       break;
+//       default : console.log('BUNDAY OY YO\'Q');
+// }
+
+
+// 4-ex
+// let monthData = +prompt('enter digit')
+
+// switch (monthData) {
+//    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+//       console.log('31-kun');
+//       break;
+//    case 4: case 6: case 9: case 11:
+//       console.log('30-kun');
+//       break;
+//    case 2:
+//       console.log('28-kun');
+//       break;
+//       default : console.log('BUNDAY OY YO\'Q');
+// }
+
+
+
+// 5-ex
+// let marksBonus = +prompt('enter digit')
+
+// switch (marksBonus) {
+//     case 1:
+//         console.log('100.000');
+//         break;
+//     case 2:
+//         console.log('200.000');
+//         break;
+//     case 3:
+//         console.log('300.000');
+//         break;
+//     case 4:
+//         console.log('400.000');
+//         break;
+
+//     case 5:
+//         console.log('500.000');
+//         break;
+//     default: console.log('SIZ STIPENDIYA OMISIZ');
+// }
